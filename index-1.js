8ae2d61a248b5ee269b1b6017e9dd32503bbcbe2
tratchapong <tratchapong@gmail.com>
2020-01-09 21:47:49 +0700

    add index-1

--- index-1.js
+++ index-1.js
@@ -1,15 +1,40 @@
-import low from 'lowdb'
-import LocalStorage from 'lowdb/adapters/LocalStorage'
 
 
-const adapter = new LocalStorage('db')
+const adapter = new LocalStorage('db1')
 const db = low(adapter)
 
-db.defaults({ posts: [] })
+db.defaults({ items: [] })
   .write()
 
-// Data is automatically saved to localStorage
-db.get('posts')
-  .push({ title: 'lowdb' })
-  .write()
+function add() {
+  db.get('items')
+    .push({ time: Date.now() })
+    .write()
+}
+
+function reset() {
+  db.set('items', [])
+    .write()
+}
+
+function render() {
+  const state = db.getState()
+  console.log(state.items)
+  const str = JSON.stringify(state, null, 2)
+  document.getElementById('state').innerHTML = str
+  // console.log(str)
+}
+
+document.getElementById('reset').onclick = function() {
+  reset()
+  render()
+}
+
+document.getElementById('add').onclick = function() {
+  add()
+  render()
+}
+
+render()
+
 
